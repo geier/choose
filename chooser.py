ca754999d05088b9219d73ac2fdb5b000a6b9626
Christian Geier <geier@lostpackets.de>
2012-11-06 16:19:22 +0100

    basically working

--- chooser.py
+++ chooser.py
@@ -1,79 +1,76 @@
 #!/usr/bin/env python
+"""simple chooser program
+
+reads lines from stdin, lets user choose one line in an urwid (ncurses like
+interface), prints it to stdout and exits. Input will be aligned to tabs '\t'
+"""
 
 import urwid
 import sys
 import os
-import string
 
 
-def getTerminalSize():
+def get_terminal_size():
     """ taken from
     http://stackoverflow.com/questions/566746/how-to-get-console-window-width-in-python/566752#566752
     by user Johannes Weiss http://stackoverflow.com/users/55925/johannes-weiss
     """
     env = os.environ
 
-    def ioctl_GWINSZ(fd):
+    def ioctl_GWINSZ(filedesc):
         try:
             import fcntl
             import termios
             import struct
-            cr = struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ,
-                               '1234'))
-        except:
+            con_props = struct.unpack('hh', fcntl.ioctl(filedesc,
+                                                        termios.TIOCGWINSZ,
+                                                        '1234'))
+        except IOError:
             return None
-        return cr
-    cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
-    if not cr:
+        return con_props
+    con_props = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
+    if not con_props:
         try:
-            fd = os.open(os.ctermid(), os.O_RDONLY)
-            cr = ioctl_GWINSZ(fd)
-            os.close(fd)
-        except:
+            filedesc = os.open(os.ctermid(), os.O_RDONLY)
+            con_props = ioctl_GWINSZ(filedesc)
+            os.close(filedesc)
+        except IOError:
             pass
-    if not cr:
+    if not con_props:
         try:
-            cr = (env['LINES'], env['COLUMNS'])
+            con_props = (env['LINES'], env['COLUMNS'])
         except:
-            cr = (25, 80)
-    return int(cr[1]), int(cr[0])
+            con_props = (25, 80)
+    return int(con_props[1]), int(con_props[0])
 
 
-class SelText(urwid.Text):
-    """
-Selectable Text with an aditional href varibale
-"""
-    def __init__(self, text):
-        urwid.Text.__init__(self, text)
-
-    def selectable(self):
-        """needs to be implemented"""
-        return True
-
-    def keypress(self, _, key):
-        """needs to be implemented"""
-        return key
-
-
-class SelectedButton(Exception):
-    def __init__(self, exit_token=None):
-        Exception.__init__(self)
-        self.exit_token = exit_token
-
-
-class Selected(Exception):
-    """
-    used for signalling that an item was chosen in urwid
-    """
-    pass
-
-
-def select_entry(names, width=80):
+def select_entry(names, header_text=''):
     """interactive href selector (urwid based)
 
     returns: href
     return type: string
     """
+    class SelText(urwid.Text):
+        """Selectable Text"""
+        def __init__(self, text):
+            urwid.Text.__init__(self, text)
+
+        @classmethod
+        def selectable(cls):
+            """needs to be implemented"""
+            return True
+
+        @classmethod
+        def keypress(cls, _, key):
+            """needs to be implemented"""
+            return key
+
+    class Selected(Exception):
+        """
+        used for signalling that an item was chosen in urwid
+        """
+        pass
+
     if len(names) is 1:
         return names[0]
     if names == list():
@@ -87,17 +84,9 @@ def select_entry(names, width=80):
         urwid.AttrMap(w, None, 'reveal focus') for w in name_list])
 
     listbox = urwid.ListBox(content)
-    show_key = urwid.Text(u"", wrap='clip')
-    head = urwid.AttrMap(show_key, 'header')
+    header = urwid.Text(header_text, wrap='clip')
+    head = urwid.AttrMap(header, 'header')
     top = urwid.Frame(listbox, head)
-
-    def show_all_input(input, raw):
-        """used for urwid test
-        to be removed
-        """
-        show_key.set_text(u"Pressed: " + u" ".join([
-            unicode(i) for i in input]))
-        return input
 
     def keystroke(input):
         """used for urwid test
@@ -110,49 +99,63 @@ def select_entry(names, width=80):
             raise Selected()
 
     loop = urwid.MainLoop(top, palette,
-                          input_filter=show_all_input,
                           unhandled_input=keystroke)
     try:
         loop.run()
     except Selected:
         return names[listbox.get_focus()[1]]
 
-##################################################
-width, height = getTerminalSize()
 
-auswahl = sys.stdin.read()
+def do_it():
+    width, _ = get_terminal_size()
+
+    auswahl = sys.stdin.read()
+
+    # save old stdout and in
+    old_out = sys.__stdout__
+    old_in = sys.__stdin__
+
+    sys.__stdout__ = sys.stdout = open('/dev/tty', 'wb')
+    sys.__stdin__ = sys.stdin = open('/dev/tty')
+    os.dup2(sys.stdin.fileno(), 0)
+
+    def get_lengths(auswahl):
+        """return max lengths of strings in 'columns' """
+        return map(max, zip(*[map(len, one) for one in auswahl]))
+
+    # main work is done here
+    auswahl = auswahl.split('\n')[:-1] # last line is always empty
+    auswahl = [one.split('\t') for one in auswahl]
+    laengen = get_lengths(auswahl)
+    number_columns = len(laengen)
+    number_splits = number_columns - 1
+
+    while sum(laengen) + number_splits > width:
+        missing = width - sum(laengen) - number_splits
+        number_offenders = 0
+        for lang in laengen:
+            if lang > width / number_columns + number_splits:
+                number_offenders = number_offenders + 1
+        for ind, lang in enumerate(laengen):
+            if lang > width / number_columns + number_splits:
+                laengen[ind] = laengen[ind] + missing / number_offenders
+                break
+
+    # set length of column[i] to length[i], either clip or extent
+    auswahl = [[s[:lange].ljust(lange + 1) for s, lange in zip(wahl, laengen)] for wahl in auswahl]
+
+    auswahl = [''.join(elemente) for elemente in auswahl]
+    auswahl = select_entry(auswahl,
+                           header_text='Who do You want to call today?')
+
+    #restore old stdout
+    sys.stdin.close()
+    sys.stdout.close()
+    sys.__stdout__ = sys.stdout = old_out
+    sys.__stdin__ = sys.stdin = old_in
+
+    # print chosen string
+    print(auswahl)
 
 
-# save old stdout and in
-old_out = sys.__stdout__
-old_in = sys.__stdin__
-
-sys.__stdout__ = sys.stdout = open('/dev/tty', 'wb')
-sys.__stdin__ = sys.stdin = open('/dev/tty')
-os.dup2(sys.stdin.fileno(), 0)
-
-
-def get_lengths(auswahl):
-    return map(max, zip(*[map(len, one) for one in auswahl]))
-
-# main work is done here
-width = 180 / 3
-auswahl = auswahl.split('\n')
-auswahl = auswahl[:-1]
-auswahl = [one.split('\t') for one in auswahl]
-laengen = get_lengths(auswahl)
-
-auswahl = [[string.ljust(s[:lange - 1], lange) for s, lange in zip(wahl, laengen)] for wahl in auswahl]
-
-auswahl = [''.join(elemente) for elemente in auswahl]
-wahl = select_entry(auswahl)
-
-#restore old stdout
-sys.stdin.close()
-sys.stdout.close()
-sys.__stdout__ = sys.stdout = old_out
-sys.__stdin__ = sys.stdin = old_in
-
-# print chosen string
-print(wahl + '\n')
-print langen
+do_it()
